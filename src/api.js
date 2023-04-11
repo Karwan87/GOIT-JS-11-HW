2c92646832fcc3d520fcb1ac83483eaa8514d721
Karwan87 <damianuszynski@gmail.com>
2023-04-11 20:06:42 +0200

    Migrating api.js to index.js

--- src/api.js
+++ src/api.js
@@ -1,37 +1,37 @@
-import axios from 'axios';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import axios from 'axios';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const API_KEY = '35038868-0cefdd0904fdf8a70a3b6f6a2';
-const PER_PAGE = 40;
+// const API_KEY = '35038868-0cefdd0904fdf8a70a3b6f6a2';
+// const PER_PAGE = 40;
 
-export async function searchImages(searchTerm, page) {
-  try {
-    const response = await axios.get('https://pixabay.com/api/', {
-      params: {
-        key: API_KEY,
-        q: searchTerm,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: PER_PAGE,
-        page: page,
-      },
-    });
-    const data = response.data;
-    const hits = data.hits;
-    console.log(hits);
+// export async function searchImages(searchTerm, page) {
+//   try {
+//     const response = await axios.get('https://pixabay.com/api/', {
+//       params: {
+//         key: API_KEY,
+//         q: searchTerm,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//         per_page: PER_PAGE,
+//         page: page,
+//       },
+//     });
+//     const data = response.data;
+//     const hits = filterImagesBySearchTerm(data.hits, searchTerm);
+//     console.log(hits);
 
-    const lightbox = new SimpleLightbox('gallery-item');
-    return {
-      hits,
-      totalHits: data.totalHits,
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      hits: [],
-      totalHits: 0,
-    };
-  }
-}
+//     const lightbox = new SimpleLightbox('gallery-item');
+//     return {
+//       hits,
+//       totalHits: data.totalHits,
+//     };
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       hits: [],
+//       totalHits: 0,
+//     };
+//   }
+// }
